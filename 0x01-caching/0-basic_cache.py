8faa9cffa2d65ba9a3fe6e6a41410b24038dc973
Bonaventure Anselm <bona.anselm@gmail.com>
2023-07-26 22:09:23 +0100

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" Defines class BasicCache """
+""" Defines BasicCache class """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """ A caching system """
+
     def put(self, key, item):
-        """ Inserts data into our dictionary """
+        """ Inserts values to cache """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """ Retrives data from our cache """
         return self.cache_data.get(key, None)
