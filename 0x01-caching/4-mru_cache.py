e29c7803fea85912d70c1f2ee6ef79338adda4f4
Bonaventure Anselm <bona.anselm@gmail.com>
2023-07-27 08:08:44 +0100

    Task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Defines MRUCache """
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """ A MRU based caching system """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Adds data to our cache """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("Discard:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
